373084ed27e87da913e19c8c38f62ae13dec73c2
Frode Lund <flund@lsk.mil.no>
2024-01-18 11:25:25 +0100

    Første commit med farget vits

--- app.js
+++ app.js
@@ -4,7 +4,4 @@ const kjuttaVits = `${chalk.red('Fortelle')} ${chalk.yellow('en')} ${chalk.green
                 ${chalk.cyan('Kjutta')}? ${chalk.blue('med')} ${chalk.magenta('slips')}`;
 
 console.log(kjuttaVits);
-git add .
-git commit -m "Første commit med farget vits"
-git remote add origin https://github.com/frlund/nodeTest
-git push -u origin master
+
