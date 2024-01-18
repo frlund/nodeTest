63eecf161a2ed698957421f07dc1bb9b2de45c74
frode lund <156180539+frlund@users.noreply.github.com>
2024-01-18 15:13:53 +0100

    app.js med farget "vits"

---
+++ app.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+const chalk = require('chalk');
+
+const kjuttaVits = `${chalk.red('Fortelle')} ${chalk.yellow('en')} ${chalk.green('vits ?')} 
+                ${chalk.cyan('Kjutta')}? ${chalk.blue('med')} ${chalk.magenta('slips')}`;
+
+console.log(kjuttaVits);
+
